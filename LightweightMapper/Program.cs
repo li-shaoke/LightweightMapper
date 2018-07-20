2b95e67a6a05da83972bc2a18c1c726737a13044
Eric <eric_li@live.cn>
2018-07-20 16:26:31 +0800

    creat

---
+++ LightweightMapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LightweightMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new A
+            {
+                Id = 1,
+                Name = "张三",
+                User = new C
+                {
+                    Id = 1,
+                    Name = "李四"
+                }
+            };
+            B b = Mapper<A, B>.Map(a);
+            Console.WriteLine(b.Id + "_" + b.Equals(a));
+
+            A a2 = Mapper<A, A>.Map(a);
+
+            Console.WriteLine(a2.Name + "_" + a2.Equals(a));
+
+        }
+    }
+}
---
+++ LightweightMapper/TestModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightweightMapper
+{
+    public class A
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public C User { get; set; }
+
+        /// <summary>
+        /// 标注为notmapped特性时，不转换赋值
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+        public D UserA { get; set; }
+
+    }
+
+    public class B
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public D User { get; set; }
+        public D UserA { get; set; }
+    }
+
+    public class C
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class D
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
